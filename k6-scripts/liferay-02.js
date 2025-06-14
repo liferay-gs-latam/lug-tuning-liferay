f7a5d82c81fc64fbade19ace017af1f4a1e9c9be
andrefabbro <andre.fabbro@liferay.com>
2025-06-14 13:41:14 -0300

    Update k6 script

--- k6-scripts/liferay-02.js
+++ k6-scripts/liferay-02.js
@@ -9,17 +9,21 @@ const users = new SharedArray('users', function () {
 });
 
 export const options = {
-  thresholds: {},
+  thresholds: {
+    http_req_failed: ['rate<0.10'], // http errors should be less than 10%
+    http_req_duration: ['p(95)<5000'], // 95% of requests should be below 5s
+  },
   scenarios: {
     Scenario_1: {
       executor: 'ramping-vus',
       gracefulStop: '30s',
       stages: [
         { target: 5, duration: '30s' },
+        { target: 20, duration: '1m' },
+        { target: 40, duration: '1m' },
         { target: 50, duration: '1m' },
-        { target: 50, duration: '1m' },
-        { target: 5, duration: '1m' },
-        { target: 0, duration: '30s' },
+        { target: 20, duration: '1m' },
+        { target: 5, duration: '30s' },
       ],
       gracefulRampDown: '30s',
       exec: 'scenario_1',
@@ -35,32 +39,8 @@ export function scenario_1() {
   // Pega um usuário aleatório (ou use uma lógica específica)
   const user = users[Math.floor(Math.random() * users.length)];
 
-  group('Home Page - /home', function () {
-    response = http.get('http://liferay:8080/home', {
-      headers: {
-        'upgrade-insecure-requests': '1',
-        'sec-ch-ua': '"Chromium";v="136", "Google Chrome";v="136", "Not.A/Brand";v="99"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"',
-      },
-    })
-
-    // Validação com check
-    check(response, {
-      'Home page status is 200': (r) => r.status === 200,
-      'Response time < 500ms': (r) => r.timings.duration < 500,
-      'Body is not empty': (r) => r.body.length > 0,
-    });
-
-    if (!response.status === 200) {
-      console.warn(`Request failed with status ${response.status}`);
-    }
-
-    sleep(0.2)
-  })
-
   group('Login Page - /c/portal/login', function () {
-    response = http.get('http://liferay:8080/c/portal/login', {
+    response = http.get('http://liferay:8080/home?p_p_id=com_liferay_login_web_portlet_LoginPortlet&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&_com_liferay_login_web_portlet_LoginPortlet_mvcRenderCommandName=%2Flogin%2Flogin&saveLastPath=false', {
       headers: {
         'upgrade-insecure-requests': '1',
         'sec-ch-ua': '"Chromium";v="136", "Google Chrome";v="136", "Not.A/Brand";v="99"',
@@ -72,13 +52,15 @@ export function scenario_1() {
     // Validação com check
     check(response, {
       'Login Page status is 200': (r) => r.status === 200,
-      'Response time < 500ms': (r) => r.timings.duration < 500,
-      'Body is not empty': (r) => r.body.length > 0,
+      'Login Page Response time < 1000ms': (r) => r.timings.duration < 1000,
+      'Login Page Body is not empty': (r) => r.body.length > 0,
     });
 
     if (!response.status === 200) {
       console.warn(`Request failed with status ${response.status}`);
     }
+
+    sleep(0.5)
 
     vars['_com_liferay_login_web_portlet_LoginPortlet_formDate1'] = response
       .html()
@@ -108,9 +90,6 @@ export function scenario_1() {
     const formAction = response.html().find('form#_com_liferay_login_web_portlet_LoginPortlet_loginForm').first().attr('action');
     const pAuthMatch = formAction.match(/p_auth=([^&]+)/);
     vars['p_auth_token'] = pAuthMatch ? pAuthMatch[1] : '';
-
-
-    sleep(0.6)
   })
 
   group('Login Post and load home',
@@ -142,15 +121,15 @@ export function scenario_1() {
       // Validação com check
       check(response, {
         'Login Post status is 200': (r) => r.status === 200,
-        'Response time < 500ms': (r) => r.timings.duration < 500,
-        'Body is not empty': (r) => r.body.length > 0,
+        'Login Post Response time < 5000ms': (r) => r.timings.duration < 5000,
+        'Login Post Body is not empty': (r) => r.body.length > 0,
       });
 
       if (!response.status === 200) {
         console.warn(`Request failed with status ${response.status}`);
       }
 
-      sleep(1.2)
+      sleep(1)
     }
   )
 }
